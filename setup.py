890a763d38cffc0ab23f6deb7169adebe9d25782
willnx <willnx84@gmail.com>
2020-02-21 10:04:52 -0800

    Added quota service to router logic

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(name="vlab-api-gateway",
       author="Nicholas Willhite,",
       author_email='willnx84@gmail.com',
-      version='2019.07.12',
+      version='2020.02.21',
       packages=find_packages(),
       description="Routes requests to vLab services",
       install_requires=['gunicorn', 'gevent', 'ujson', 'cffi>=1.11.5'],
